ccb7cbe0e6ff6ac24931127872886b475452d42a
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-07-29 18:51:56 +0300

    initial commit

---
+++ src/main/java/org/example/jam_bookstore/JamBookstoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.jam_bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JamBookstoreApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/jam_bookstore/controller/BookController.java
@@ -0,0 +1,7 @@
+package org.example.jam_bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController {
+}
