efa6041127b67fcaa8f0fb304bc89b485523fd28
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-07-31 11:07:50 +0300

    add controller, entity, repository, service

--- src/main/java/org/example/jam_bookstore/controller/BookController.java
+++ src/main/java/org/example/jam_bookstore/controller/BookController.java
@@ -1,7 +1,12 @@
 package org.example.jam_bookstore.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 }
---
+++ src/main/java/org/example/jam_bookstore/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package org.example.jam_bookstore.repository;
+
+import org.example.jam_bookstore.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
